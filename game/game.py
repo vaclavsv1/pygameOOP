148d719711332a15bda1a5d824906c6667a29df4
Václav Švec <Vaclav.Svec@student.souepl.cz>
2026-02-02 11:34:23 +0100

    game
    1

---
+++ game/game.py
@@ -0,0 +1,31 @@
+import pygame
+from config import *
+
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            print(event.type)
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+
+                self.running == False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill(SKY_BLUE)
+        pygame.display.flip()
+
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            self.clock.tick(FPS)
