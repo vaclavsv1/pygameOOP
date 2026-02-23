935388881172486ec7835c7760ab393a1898ed96
Václav Švec <Vaclav.Svec@student.souepl.cz>
2026-02-23 13:37:43 +0100

    5
    5

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from game.game import Game
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("GOATed Player")
+    pygame.display.set_caption("GOATED")
 
     game = Game(screen)
     game.run()
